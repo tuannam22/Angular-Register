d57b57fdb67f4243f998f2e1881c702ba21a2aaa
tuannam22 <57036608+tuannam22@users.noreply.github.com>
2019-10-26 20:00:45 +0700

    Create register.module.ts

---
+++ register.module.ts
@@ -0,0 +1,35 @@
+//Services->register.module.ts
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable()
+
+export class RegisterService{
+
+    constructor(  private http:HttpClient ){}
+
+    getRegister(username, password, name, email){
+
+        //option
+        let options = {
+            headers: new HttpHeaders().set("Content-Type","application/x-www-form-urlencoded")
+        }
+
+        //body
+        let body = new URLSearchParams();
+        body.set("username", username);
+        body.set("password", password);
+        body.set("name", name);
+        body.set("email", email);
+
+        return this.http.post("http://localhost:3000/api/register", body.toString(), options)
+    }
+    
+    login(){
+
+    }
+
+    checklogin(){
+        
+    }
+}
