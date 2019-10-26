23a18b831b16867c86ec25b671d8bbdd44709acc
tuannam22 <57036608+tuannam22@users.noreply.github.com>
2019-10-26 19:57:36 +0700

    Create user.component.ts

---
+++ user.component.ts
@@ -0,0 +1,12 @@
+Trong component->blocks->Users
+
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl:"user.component.html",
+    selector:'user'
+})
+
+export class UserComponent{
+    
+}
