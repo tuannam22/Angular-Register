a19283aebc3b93d69ce9191c7f0323546c754e5f
tuannam22 <57036608+tuannam22@users.noreply.github.com>
2019-10-26 19:50:38 +0700

    Create register.component.ts

---
+++ register.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RegisterService } from '../../../Services/Register.module';
+
+@Component({
+    templateUrl:"register.component.html",
+    selector:'register',
+    providers:[RegisterService]
+})
+
+export class RegisterComponent{
+
+    thongbao:Boolean=false;
+    noidung:string= "";
+
+    constructor( private Register:RegisterService){}
+        registerform(f){
+            // this.noidung="thanh cong";
+            // this.thongbao=true;
+            // console.log(f);            
+            this.Register.getRegister(f.form.value.txtUsername,f.form.value.txtPassword,f.form.value.txtName,f.form.value.txtEmail).subscribe(data=>{
+                // console.log(data);
+                this.thongbao=true;
+                this.noidung="dang ki  thanh cong";
+            });
+        }  
+}
